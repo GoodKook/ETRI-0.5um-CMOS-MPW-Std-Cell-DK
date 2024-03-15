14927a7499b532e461812e42c44d6aedec860c4e
GoodKook <goodkook@gmail.com>
2024-03-16 07:17:27 +0900

    Update

---
+++ Tools/gsl_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+echo "ngSpice-42 Quick Build & Install"
+echo "--------------------------------"
+# Prerequisites:
+#source prerequisities.sh
+
+if [ ! -f ./gsl-latest.tar.gz ]; then
+    wget https://mirror.ibcp.fr/pub/gnu/gsl/gsl-latest.tar.gz
+fi
+tar xvf gsl-latest.tar.gz
+
+cd gsl-*
+./configure
+make
+sudo make install
+make clean
+cd ..
---
+++ Tools/python_libs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+echo "Python Packages"
+echo "----------------"
+
+python3 -m pip install -U pip
+python3 -m pip install -U numpy
+python3 -m pip install -U matplotlib
+python3 -m pip install -U PILLOW
