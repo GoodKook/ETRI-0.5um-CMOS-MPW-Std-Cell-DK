89629bd5e33fb07adc9f73118c6114d8a6d24546
GoodKook <goodkook@gmail.com>
2024-08-11 13:15:01 +0900

    Updated

--- Tutorials/2-5_Lab3_FIR8/2_Splited_IO/simulation/obj_dir/cnoise.d
+++
@@ -1,2 +0,0 @@
-cnoise.o: ../../../0_algorithm/c_untimed/cnoise.cpp \
-  ../../../0_algorithm/c_untimed/cnoise.h
--- Tutorials/2-5_Lab3_FIR8/2_Splited_IO/simulation/obj_dir/fir8.d
+++
@@ -1,2 +0,0 @@
-fir8.o: ../../../0_algorithm/c_untimed/fir8.cpp \
-  ../../../0_algorithm/c_untimed/fir8.h
