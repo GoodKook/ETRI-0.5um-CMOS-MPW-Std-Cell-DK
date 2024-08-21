95a05f51071cb844550ea8c720a6e789c9f999ac
GoodKook <goodkook@gmail.com>
2024-08-21 12:18:51 +0900

    Update qflow-1.4.100_etri050_build.sh

--- Tools/qflow-1.4.100_etri050_build.sh
+++ Tools/qflow-1.4.100_etri050_build.sh
@@ -27,15 +27,15 @@ sudo mv etri050 etri050.bak
 sudo mkdir etri050
 cd etri050
 sudo ln -s ~/ETRI050_DesignKit/tech/SCN3ME_SUBM.30.ETRI.tech SCN3ME_SUBM.30.ETRI.tech
-sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri050.magicrc etri050.magicrc
 sudo ln -s ~/ETRI050_DesignKit/tech/etri050.par etri050.par
-sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri050.prm etri050.prm
 sudo ln -s ~/ETRI050_DesignKit/tech/etri050.sh etri050.sh
 sudo ln -s ~/ETRI050_DesignKit/tech/etri050_setup.tcl etri050_setup.tcl
+sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/khu_etri050.magicrc etri050.magicrc
+sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/khu_etri050.prm etri050.prm
 sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri050_stdcells.lef etri050_stdcells.lef
-sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri050_stdcells.sp etri050_stdcells.sp
-sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri05_stdcells.gds2 etri05_stdcells.gds2
-sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri05_stdcells.lib etri05_stdcells.lib
-sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/etri05_stdcells.v etri05_stdcells.v
+sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/khu_etri050_stdcells.spice etri050_stdcells.sp
+sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/khu_etri050_stdcells.gds2 etri05_stdcells.gds2
+sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/khu_etri05_stdcells.lib etri05_stdcells.lib
+sudo ln -s ~/ETRI050_DesignKit/digital_ETRI/khu_etri05_stdcells.v etri05_stdcells.v
 
 
