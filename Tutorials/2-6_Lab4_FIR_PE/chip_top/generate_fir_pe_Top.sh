0212a5e24f25a8916df9fcfdc2fba5af3f03c2a4
GoodKook <goodkook@gmail.com>
2024-09-04 20:57:10 +0900

    updated

--- Tutorials/2-6_Lab4_FIR_PE/chip_top/generate_fir_pe_Top.sh
+++ Tutorials/2-6_Lab4_FIR_PE/chip_top/generate_fir_pe_Top.sh
@@ -2,4 +2,4 @@
 
 ~/ETRI050_DesignKit/scripts/xPad.py fir_pe
 ~/ETRI050_DesignKit/scripts/xPin_Route.py fir_pe
-~/ETRI050_DesignKit/scripts/generate_chip.sh fir_pe 97.5 97.5 441.0 441.0 484.250 541.850
+~/ETRI050_DesignKit/scripts/generate_chip.sh fir_pe 97.5 97.5 441.0 441.0 508.5 541.5
