203be87156e1b66de982b7d7472b3ea3953229de
GoodKook <goodkook@gmail.com>
2024-10-06 15:12:27 +0900

    update.tech

---
+++ scripts/mag2gds.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Export Magic layout to flattened GDS without label"
+    echo "usage: mag2gds.sh <magic>" >&2
+    exit 2
+fi
+
+echo "************************************************************************"
+echo "* Generate flatten layout for Core using ETRI Rule"
+echo "************************************************************************"
+if ! [ -f $1.mag ]; then
+  echo "Magic file not exist"
+  exit
+fi
+magic -dnull -noconsole << EOF
+drc off
+box 0 0 0 0
+load $1.mag -force
+drc off
+select top cell
+expand
+flatten $1_F
+load  $1_F
+cif ostyle lambda=0.30(ETRI)
+gds drccheck no
+gds flatten yes
+gds labels no
+gds write $1_F
+quit -force
+EOF
+
+
