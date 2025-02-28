bad55d794cb6c8c0cb38146e8a84271ce9fe9d4f
GoodKook <goodkook@gmail.com>
2025-02-28 17:40:17 +0900

    updated

--- analog_ETRI/load_ana_gds.tcl
+++ analog_ETRI/load_ana_gds.tcl
@@ -1,4 +1,4 @@
 cif istyle lambda=0.30(ETRI-G)
 cif ostyle lambda=0.30(ETRI-G)
-gds read ~/ETRI050_DesignKit/analog_ETRI/GDS/NSPL_0p5um.gds
+gds read ~/ETRI050_DesignKit/analog_ETRI/GDS/NSPL_0p5um_Klayout.gds
 
