a43c3a641da2be9cdec711ec8839417c5d8a3594
GoodKook <goodkook@gmail.com>
2025-12-13 11:51:52 +0900

    Updated

--- Projects/HLS/basic_loops/ETRI050/qflow_exec.sh
+++ Projects/HLS/basic_loops/ETRI050/qflow_exec.sh
@@ -3,12 +3,12 @@
 # qflow exec script for project ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050
 #-------------------------------------------
 
-/usr/local/share/qflow/scripts/yosys.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050/source/basic_loops_flow_control_loop_pipe.v || exit 1
+# /usr/local/share/qflow/scripts/yosys.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050/source/basic_loops_flow_control_loop_pipe.v || exit 1
 # /usr/local/share/qflow/scripts/graywolf.sh -d ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
 # /usr/local/share/qflow/scripts/qrouter.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
 # /usr/local/share/qflow/scripts/vesta.sh  -d ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
-# /usr/local/share/qflow/scripts/magic_db.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
+/usr/local/share/qflow/scripts/magic_db.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
 # /usr/local/share/qflow/scripts/magic_drc.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
 # /usr/local/share/qflow/scripts/netgen_lvs.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
 # /usr/local/share/qflow/scripts/magic_gds.sh ~/ETRI050_DesignKit/devel/Projects/HLS/basic_loops/ETRI050 basic_loops || exit 1
--- Projects/HLS/basic_loops/basic_loops/hls_component/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,5 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "basic_loops", "RefName" : "basic_loops","ID" : "0","Type" : "pipeline",
-"SubLoops" : [
-	{"Name" : "LOOP_1","RefName" : "LOOP_1","ID" : "1","Type" : "pipeline"},]
-}]}
