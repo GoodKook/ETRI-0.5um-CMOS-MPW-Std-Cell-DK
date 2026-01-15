ba3f1101e728494487497876715ab04e81390979
GoodKook <goodkook@gmail.com>
2026-01-15 10:19:35 +0900

    Update cpu_6502

---
+++ Projects/RTL/cpu_6502/ETRI050/qflow_exec.sh
@@ -0,0 +1,17 @@
+#!/bin/tcsh -f
+#-------------------------------------------
+# qflow exec script for project ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050
+#-------------------------------------------
+
+/usr/local/share/qflow/scripts/yosys.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050/source/cpu_6502.v || exit 1
+# /usr/local/share/qflow/scripts/graywolf.sh -d ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/vesta.sh  ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/qrouter.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/vesta.sh  -d ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/magic_db.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/magic_drc.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/netgen_lvs.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/magic_gds.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/cleanup.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/cleanup.sh -p ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
+# /usr/local/share/qflow/scripts/magic_view.sh ~/ETRI050_DesignKit/devel/Projects/RTL/cpu_6502/ETRI050 cpu_6502 || exit 1
