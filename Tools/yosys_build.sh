d9e0f84ee16644db0263d316f11c72d4080a8862
GoodKook <goodkook@gmail.com>
2026-02-05 21:59:26 +0900

    Updated

--- Tools/yosys_build.sh
+++ Tools/yosys_build.sh
@@ -6,16 +6,22 @@ echo "---------------------------"
 
 if [ ! -d ./yosys ]; then
     # only once
-    git clone https://github.com/YosysHQ/yosys.git
+    git clone --recurse-submodules https://github.com/YosysHQ/yosys.git
 fi
 
 cd yosys
 git pull         # Make sure git repository is up-to-date
-git submodule update --init
+git submodule update --init --recursive
 
 #unset CXXFLAGS
-export CXX=clang++
-export CXXFLAGS=-std=c++17
+#export CXX=clang++
+#export CXXFLAGS=-std=c++17
+
+# Prerequisites:
+sudo apt-get install gawk git make python3 lld bison clang flex \
+	libffi-dev libfl-dev libreadline-dev pkg-config tcl-dev zlib1g-dev \
+	graphviz xdot
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
 make config-clang
 make -j`nproc`
